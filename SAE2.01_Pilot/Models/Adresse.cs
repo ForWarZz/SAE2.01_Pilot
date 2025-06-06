27b94f8abd958ad71cae0df944d0c8b2996a8675
Morgan Battig <Morgan.Battig@etu.univ-smb.fr>
2025-06-06 14:34:00 +0200

    Ajout model Revendeur et Adresse

---
+++ SAE2.01_Pilot/Models/Adresse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE2._01_Pilot.Models
+{
+    public class Adresse
+    {
+        public string Rue { get; set; }
+        public string Ville { get; set; }
+
+        private string codePostal;
+
+        public string CodePostal { 
+            get => codePostal;
+
+            set
+            {
+                if (value.Length < 5)
+                {
+                    throw new ArgumentOutOfRangeException("Le code postal n'est pas valide.");
+                }
+
+                codePostal = value;
+            }
+        }
+    }
+}
