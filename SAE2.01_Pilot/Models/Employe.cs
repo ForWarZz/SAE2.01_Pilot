ed12f8558ca38dfabceeafca28c7dde2eca315cf
Morgan Battig <Morgan.Battig@etu.univ-smb.fr>
2025-06-06 14:49:40 +0200

    Ajout model Employe + role

---
+++ SAE2.01_Pilot/Models/Employe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE2._01_Pilot.Models
+{
+    public class Employe
+    {
+        public int Id { get; set; }
+        public string Nom {  get; set; }
+        public string Prenom { get; set; }
+        public Role Role { get; set; }
+
+        public Employe()
+        {
+            
+        }
+
+        public Employe(int id, string nom, string prenom, Role role)
+        {
+            Id = id;
+            Nom = nom;
+            Prenom = prenom;
+            Role = role;
+        }
+    }
+}
---
+++ SAE2.01_Pilot/Models/Role.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE2._01_Pilot.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public LibelleRole LibelleRole { get; set; }
+    }
+
+    public enum LibelleRole
+    {
+        Commercial,
+        ResponsableProduction
+    }
+}
