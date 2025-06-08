035cdd11d7c545405c9221356648ff8120c880d9
Morgan Battig <Morgan.Battig@etu.univ-smb.fr>
2025-06-08 19:13:25 +0200

    Ajout loading des commandes

--- SAE2.01_Pilot/UserControls/UCCommandes.xaml.cs
+++ SAE2.01_Pilot/UserControls/UCCommandes.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SAE2._01_Pilot.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,19 @@ namespace SAE2._01_Pilot.UserControls
     /// </summary>
     public partial class UCCommandes : UserControl
     {
+        public ObservableCollection<Commande> Commandes { get; set; }
+
         public UCCommandes()
         {
+            // ChargerCommandes();
             InitializeComponent();
+
+            // dgCommandes.DataContext = Commandes;
+        }
+
+        private void ChargerCommandes()
+        {
+            Commandes = new ObservableCollection<Commande>(Commande.GetAll());
         }
     }
 }
