719857effa4e3367601f085e30072395a928a6c1
Axel Benard <Axel.Benard@etu.univ-smb.fr>
2025-06-12 16:49:39 +0200

    diagramme de classe + classe popup (pas fini (même pas commencé en vrai))

---
+++ SAE2.01_Pilot/Windows/PopUp.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SAE2._01_Pilot.Windows
+{
+    /// <summary>
+    /// Logique d'interaction pour PopUp.xaml
+    /// </summary>
+    public partial class PopUp : Window
+    {
+        public PopUp()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+                DragMove();
+        }
+    }
+}
